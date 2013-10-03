7c9398ab157da8e465a4cf54433c44a1c3bac24b
mlyne <mlyne.careers@gmail.com>
2013-10-03 14:35:09 +0100

    clean upload of content

---
+++ ulss_001_a.js
@@ -0,0 +1,15 @@
+/*--- Avanquest WebEasy Motion Script ---*/
+
+
+function OnWeLoad()
+{
+	IDP.my=(window.parent.frames.length && parent.MyFrm)?parent.MyFrm:window;
+	IDP[1]=(V5)?'document.getElementById(\'a1\').style.color':(IE)?'a1.style.color':'document.e1.document.linkColor';
+	IDP[2]=(V5)?'document.getElementById(\'a12\').style.color':(IE)?'a12.style.color':'document.e12.document.linkColor';
+	IDP[3]=(V5)?'document.getElementById(\'a13\').style.color':(IE)?'a13.style.color':'document.e13.document.linkColor';
+	IDP[4]=(V5)?'document.getElementById(\'a14\').style.color':(IE)?'a14.style.color':'document.e14.document.linkColor';
+	isOvr=1;
+}
+
+
+/*--- EndOfFile ---*/
